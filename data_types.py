be74ccaed08b1b44d02c7b9ca7dfc5ef4cc28f54
abidex4yemi <abidex4yemi.com>
2019-09-28 16:48:53 +0100

    Initial commit

---
+++ data_types.py
@@ -0,0 +1,39 @@
+# Part 1
+x = 42
+type(x)
+
+y = 3 / 4
+type(y)
+
+z = int('7')
+type(z)
+
+a = float(5)
+type(a)
+
+name = "Nina"
+type(name)
+
+# Part 2
+rent = 480
+per_day = rent / 30
+print(per_day)
+
+# Part 3
+print("Hello world")
+name = "Yemi"
+print("My name is", name)
+
+name = "Yemi"
+print("Hello, my name is %s" % name)
+
+name = "Yemi"
+print(f"Hello, my name is {name} and I pay ${rent / 30} in rent per day")
+
+# Part 4
+x = 42
+y = 3 / 4
+name = "Yemi"
+type(x)
+type(y)
+type(name)
