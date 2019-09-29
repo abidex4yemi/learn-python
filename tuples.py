614b7400eeb3887bc548fbf0dd8f869e6ccdec0e
abidex4yemi <abidex4yemi.com>
2019-09-29 09:28:52 +0100

    Complete tuples exercise

--- tuples.py
+++ tuples.py
@@ -5,20 +5,41 @@
 # Tuples are immutable: once created the items in it cant be change
 
 a = ()
-print(type(a))
+# print(type(a))
 
 # Note: comma is mandatory and parenthesis can be omitted
 b = (1,)
-print(type(b))
+# print(type(b))
 
 numbers = 1, 2, 3, 4, 5, 6, 67, 8
-print(numbers)
+# print(numbers)
 
 students = ("Mercy", 8, "History", 3.5)
-print(students[0])
-print(students[1])
+# print(students[0])
+# print(students[1])
 
 # tuples immutability
 # this will throw this error
 # ypeError: 'tuple' object does not support item assignment
 # students[0] = "Yemi"
+
+
+####################
+# Unpacking tuples #
+####################
+name, age, subject, gpa = students
+# print(name, age, subject, gpa)
+
+name, _, _, _, = students
+# print(name)
+print(students.index("Mercy"))
+
+
+# Returning tuple from a function
+def http_status_code():
+    return 200, "Ok"
+
+
+# Unpacking tuple from a function
+status_code, status_text = http_status_code()
+print(status_code, status_text)
