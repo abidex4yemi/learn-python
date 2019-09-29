db6cac74ca50cc1cf19a2fcbac65003fec8bfaf3
abidex4yemi <abidex4yemi.com>
2019-09-29 19:01:28 +0100

    Complete control flow exercice

---
+++ control_flow_statement.py
@@ -0,0 +1,22 @@
+################################
+## Basic control flow statement#
+################################
+
+if 3 > 5:
+    print("Hello")
+else:
+    print("Hey")
+
+a = [1]
+if a:
+    print(a)
+
+a = True
+b = False
+
+if b:
+    print("Hello")
+elif a:
+    print("Hey")
+else:
+    print("Goodbye")
