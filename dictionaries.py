62316e178d3827e0a05f82f15d915ce7eee6e4c8
abidex4yemi <abidex4yemi.com>
2019-09-29 13:20:32 +0100

    Complete dictionaries exercise

--- dictionaries.py
+++ dictionaries.py
@@ -25,3 +25,39 @@ mix_values = {"1": [10, 4], 2: ("Yemi", "Jane")}
 
 # setting default value if value does not exist
 # print(mix_values.get("Three", "Key does not exist"))
+
+
+#####################################################
+# Adding, Removing, Accessing value in Dictionaries #
+#####################################################
+
+students = {"One": "Jane", "Two": "John"}
+
+# Adding #
+students["Three"] = "Yemi"
+# print(students)
+
+# This will replace existing value
+students["Two"] = "New student"
+# print(students)
+
+# Using in keyword to check if key exist
+# print("One" in students)
+
+# Join two dictionaries together
+colors = {"r": "Red", "g": "Green"}
+numbers = {"One": 1, "Two": 2, "r": "Red"}
+colors.update(numbers)
+# print(colors)
+
+vegetables = {"Green": ["Spinach"]}
+vegetables["Green"].append("Apples")
+# print(vegetables["Green"])
+
+numbers = {"One": 1, "Two": 2, "Three": 3, "Four": 4}
+print(numbers.keys())
+print(numbers.values())
+print(numbers.items())
+
+one, two, three, four = numbers.items()
+print(one, two, three, four)
