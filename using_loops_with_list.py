4f17fb58eae200808886d6730d58e26d47179ef2
abidex4yemi <abidex4yemi.com>
2019-09-29 18:08:37 +0100

    Complete looping over list exercise

---
+++ using_loops_with_list.py
@@ -0,0 +1,14 @@
+#######################
+## Looping over list ##
+#######################
+
+colors = ["Red", "Green", "Blue", "Orange"]
+
+for color in colors:
+    print(f"The color is {color}")
+
+for num in range(5):
+    print(num)
+
+for num in range(1, 7, 2):
+    print(num)
