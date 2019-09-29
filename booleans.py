76fa52ac56bc2fc941765ce2754acd176b85d28c
abidex4yemi <abidex4yemi.com>
2019-09-29 15:37:33 +0100

    Complete basic boolean demo

---
+++ booleans.py
@@ -0,0 +1,23 @@
+############################
+# Basic boolean practice   #
+############################
+print(True)
+print(False)
+
+# 0 is falsy
+print(bool(0))
+
+# any other number is truthy
+print(bool(1))
+
+# all empty container type are falsy
+print(bool([]))
+print(bool({}))
+print(bool(()))
+print(bool(set()))
+
+# None type is false
+print(bool(None))
+
+# Empty string is falsy
+print(bool(""))
