ee024eb42981020edafc504f30a018ba636bdcdf
abidex4yemi <abidex4yemi.com>
2019-09-30 12:13:40 +0100

    Create basic github top repo finder using the request package

--- real_world_usage/dog.py
+++ real_world_usage/dog.py
@@ -2,7 +2,8 @@
 import requests
 
 api_url = "http://shibe.online/api/shibes?count=1"
-response = requests.get(api_url)
+response = requests.get(api_url, {"count": 10})
 
 print(f"Response status code is: {response.status_code}")
-print(response.json())
+image_url = response.json()
+print(image_url)
