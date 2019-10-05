6d0001ccc3d58a2d882834cac1118719871d6958
abidex4yemi <abidex4yemi.com>
2019-10-05 12:05:48 +0100

    Complete class instances exercise

--- Python_intermediate/oop/cars.py
+++ Python_intermediate/oop/cars.py
@@ -1,7 +1,9 @@
 class Car:
-    def __init__(self, name, runs=True):
+    runs = True
+    number_of_wheels = 4
+
+    def __init__(self, name):
         self.name = name
-        self.runs = runs
 
     def start(self):
         if self.runs:
@@ -9,8 +11,18 @@ class Car:
         else:
             print(f"{self.name} is broken :)!")
 
+    @classmethod
+    def get_number_of_wheels(cls):
+        print(cls.number_of_wheels)
 
+
+# Instanting a class
 toyota = Car("Toyota")
+
+# Calling instance method
 toyota.start()
-toyota.runs = False
 toyota.start()
+
+# Calling class methods
+toyota.get_number_of_wheels()
+Car.get_number_of_wheels()
