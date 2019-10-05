3d25050f252035ebb3b83f8811dec24510068037
abidex4yemi <abidex4yemi.com>
2019-10-05 14:28:34 +0100

    Complete exception exercise

--- Python_intermediate/exceptions.py
+++ Python_intermediate/exceptions.py
@@ -1,19 +1,19 @@
 
-user_input = "w"
+# user_input = "w"
 
-try:
-    number = int(user_input)
-    print("Great number!", number)
-except ValueError:
-    print("No way!, thats not a number")
+# try:
+#     number = int(user_input)
+#     print("Great number!", number)
+# except ValueError:
+#     print("No way!, thats not a number")
 
-# Catching multiple error
-user_input = "w"
-d = {1: 1}
-try:
-    number = int(user_input)
-    print(d[number])
-except ValueError:
-    print("Provide a valid number")
-except KeyError:
-    print("No way invalid key")
+# # Catching multiple error
+# user_input = "w"
+# d = {1: 1}
+# try:
+#     number = int(user_input)
+#     print(d[number])
+# except ValueError:
+#     print("Provide a valid number")
+# except KeyError:
+#     print("No way invalid key")
