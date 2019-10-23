3628dd81a61a5e191c09fcb12ac5ce4cba92219b
abidex4yemi <abidex4yemi@gmail.com>
2019-10-23 13:11:16 +0100

    Practise django framework

---
+++ test_django/blog/models.py
@@ -0,0 +1,22 @@
+from django.db import models
+
+# Create your models here.
+
+
+class Author(models.Model):
+    name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
+
+
+class Article(models.Model):
+    title = models.CharField(max_length=255)
+    body = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    author = models.ForeignKey(
+        Author, on_delete=models.CASCADE, related_name="articles")
+
+    def __str__(self):
+        return self.title
---
+++ test_django/blog/urls.py
@@ -0,0 +1,6 @@
+from django import path
+from .views import hello_world
+
+urlpatterns = [
+    path('', hello_world, name="home"),
+]
